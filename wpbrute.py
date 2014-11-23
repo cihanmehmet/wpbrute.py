ad397b2a280e87898fb7a2412bf6a971fbfaf155
b3mb4m <b3mb4m@gmail.com>
2014-11-23 15:54:49 +0200

    Update wpbrute.py

--- wpbrute.py
+++ wpbrute.py
@@ -10,6 +10,7 @@ import subprocess
 #Concat : b3mb4m@mail.com
 #Youtube : http://www.youtube.com/watch?v=lZxzh-DgXW8
 #Example : wpbrute.py --url http://127.0.0.1/wordpress/wp-login.php  --username b3mb4m --list test.txt
+#I was did some mistakes will be release v2 soon !  
 
 
 class B3mB4m(object):
